4d283aba8a3124a8312f716ee492e83ba9f0eeeb
TurtlePU <sokolov.p64@gmail.com>
2022-09-02 14:42:19 +0300

    + AppState, repeat command

--- app/Main.hs
+++ app/Main.hs
@@ -5,12 +5,17 @@ module Main where
 
 import Bindings
 import Command
+import Control.Monad (void)
 import Control.Monad.State.Strict
 import System.Console.Haskeline
-import Control.Monad (void)
+
+data AppState = App {bindings :: Bindings, lastCommand :: Command}
+
+writeCmd :: Command -> AppState -> AppState
+writeCmd c s = s { lastCommand = c }
 
 main :: IO ()
-main = evalStateT (runInputT settings loop) empty
+main = evalStateT (runInputT settings loop) (App empty Repeat)
   where
     settings =
       Settings
@@ -19,19 +24,21 @@ main = evalStateT (runInputT settings loop) empty
           autoAddHistory = True
         }
 
-completeFromBindings :: CompletionFunc (StateT Bindings IO)
+completeFromBindings :: CompletionFunc (StateT AppState IO)
 completeFromBindings = completeWord escapeChar whitespace impl
   where
     escapeChar = Nothing
     whitespace = " ()\\>"
-    impl s = map simpleCompletion . matchingKeys s <$> get
+    impl s = map simpleCompletion . matchingKeys s . bindings <$> get
 
-loop :: InputT (StateT Bindings IO) ()
+loop :: InputT (StateT AppState IO) ()
 loop =
   getInputLine "> " >>= \case
     Nothing -> exit
     Just line -> do
-      let command = parseCommand line
+      command <- lift $ case parseCommand line of
+        Repeat -> lastCommand <$> get
+        cmd -> cmd <$ modify (writeCmd cmd)
       result <- case command of
         (Bind s te) -> return (Just "TODO")
         ShowBindings -> return (Just "TODO")
@@ -39,7 +46,7 @@ loop =
         (Load lm ss) -> return (Just "TODO")
         Reload -> return (Just "TODO")
         (SetPrompt s) -> return (Just "TODO")
-        Repeat -> return (Just "TODO")
+        Repeat -> return (Just noLastCommand)
         Help -> return (Just helpText)
         Quit -> return Nothing
       case result of
@@ -47,27 +54,24 @@ loop =
         Nothing -> exit
   where
     exit = void $ outputStrLn "Leaving Lambda."
-    helpText = "\
-\ Commands available from the prompt:\n\n\
-
-\   <statement>                 evaluate/run <statement> (TODO)\n\
-\   :                           repeat last command (TODO)\n\
-\   :{\\n ..lines.. \\n:}\\n       multiline command (TODO)\n\
-\   :help, :?                   display this list of commands\n\
-\   :module [+] <module> ...  \
-                            \set the context for expression evaluation (TODO)\n\
-\   :quit                       exit Lambda (TODO)\n\
-\   :reload                     reload the current module set (TODO)\n\n\
-
-\ -- Commands for debugging:\n\n\
-
-\   :trace <expr>               evaluate <expr> with tracing on (TODO)\n\n\
-
-\ -- Commands for changing settings:\n\n\
-
-\   :set prompt <prompt>        set the prompt used in Lambda (TODO)\n\n\
-
-\ -- Commands for displaying information:\n\n\
-
-\   :show bindings              \
-                        \show the current bindings made at the prompt (TODO)\n"
+    noLastCommand =
+      "there is no last command to perform\n\
+      \use :? for help."
+    helpText =
+      " Commands available from the prompt:\n\n\
+      \   <statement>                 evaluate/run <statement> (TODO)\n\
+      \   :                           repeat last command (TODO)\n\
+      \   :{\\n ..lines.. \\n:}\\n       multiline command (TODO)\n\
+      \   :help, :?                   display this list of commands\n\
+      \   :module [+] <module> ...  \
+      \set the context for expression evaluation (TODO)\n\
+      \   :quit                       exit Lambda (TODO)\n\
+      \   :reload                     reload the current module set (TODO)\n\n\
+      \ -- Commands for debugging:\n\n\
+      \   :trace <expr>               \
+      \evaluate <expr> with tracing on (TODO)\n\n\
+      \ -- Commands for changing settings:\n\n\
+      \   :set prompt <prompt>        set the prompt used in Lambda (TODO)\n\n\
+      \ -- Commands for displaying information:\n\n\
+      \   :show bindings              \
+      \show the current bindings made at the prompt (TODO)\n"
