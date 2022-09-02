c1554cadc93316395ba20ae6243ecb4282cb9a80
TurtlePU <sokolov.p64@gmail.com>
2022-09-02 21:27:23 +0300

    ^ help text

--- app/Command.hs
+++ app/Command.hs
@@ -69,12 +69,12 @@ parseCommand s = Just $ case words s of
     help =
       " Commands available from the prompt:\n\n\
       \   <statement>                 evaluate/run <statement> (TODO)\n\
-      \   :                           repeat last command (TODO)\n\
+      \   :                           repeat last command\n\
       \   :{\\n ..lines.. \\n:}\\n       multiline command (TODO)\n\
       \   :help, :?                   display this list of commands\n\
       \   :module [+] <module> ...  \
       \set the context for expression evaluation (TODO)\n\
-      \   :quit                       exit Lambda (TODO)\n\
+      \   :quit                       exit Lambda\n\
       \   :reload                     reload the current module set (TODO)\n\n\
       \ -- Commands for debugging:\n\n\
       \   :trace <expr>               evaluate <expr> with tracing on (TODO)\n\n\
