07faa0a8b6022ac92c7b354f02e150bc94ee6945
TurtlePU <sokolov.p64@gmail.com>
2022-10-19 02:59:19 +0300

    refactoring Main

--- app/Main.hs
+++ app/Main.hs
@@ -7,12 +7,14 @@ import Control.Monad.State.Strict
 import Data.Foldable (for_)
 import Data.Functor (($>))
 import Data.Labeled (Labeled (Label))
+import Data.Maybe (catMaybes)
 import Data.StringTrie
 import Data.Term hiding (App)
 import Data.Traversable (for)
 import Parser
 import System.Console.Haskeline
 import System.IO (readFile)
+import Text.Megaparsec (errorBundlePretty)
 
 ----------------------------------- AppState -----------------------------------
 
@@ -21,8 +23,8 @@ data AppState = App {bindings :: StringTrie Term, lastCommand :: Command}
 matchingKeys :: String -> AppState -> [String]
 matchingKeys s = keys . submap s . bindings
 
-writeBinding :: String -> Term -> AppState -> AppState
-writeBinding n t (App bs lc) = App (insert n t bs) lc
+setBinding :: String -> Term -> AppState -> AppState
+setBinding n t (App bs lc) = App (insert n t bs) lc
 
 writeCmd :: Command -> AppState -> AppState
 writeCmd c s = s {lastCommand = c}
@@ -32,6 +34,9 @@ allBindings = map (uncurry Label) . toList . bindings
 
 resolve' :: Term -> AppState -> Either ResolveError Term
 resolve' t = flip resolve t . bindings
+
+writeNormalized :: MonadState AppState m => String -> Term -> m ()
+writeNormalized s = modify . setBinding s . bigStep (smallStep normal)
 
 ------------------------------------- REPL -------------------------------------
 
@@ -54,33 +59,6 @@ completeFromBindings = completeWord escapeChar whitespace impl
     escapeChar = Just prefix
     whitespace = " ()\\>"
 
-type FileName = String
-type FileContents = String
-
-data BindingError = Resolve ResolveError | Parse ParsecError deriving Show
-
-updateBindings :: MonadState AppState m => [Labeled Term] -> m [BindingError]
-updateBindings [] = return []
-updateBindings ((Label nm te): xs) = do
-      state <- get
-      case resolve' te state of
-        Left e -> do
-          res <- updateBindings xs
-          return (Resolve e: res)
-        Right t -> (modify . writeBinding nm $ bigStep (smallStep normal) t) *> updateBindings xs
-
-include :: MonadState AppState m => FileName -> FileContents -> m [BindingError]
-include file s = do
-  let (errors, bindings) = parseModule file s
-  bindingErrors <- updateBindings bindings
-  let parsingErrors = map Parse errors
-  return $ bindingErrors ++ parsingErrors
-
-importModule :: FileName -> InputT (StateT AppState IO) [BindingError]
-importModule moduleName = do
-  content <- lift $ lift $ readFile ("./lib/" ++ moduleName ++ ".lc")
-  lift $ include moduleName content
-
 loop :: InputT (StateT AppState IO) ()
 loop = do
   line <- getInputLine "> "
@@ -93,10 +71,7 @@ loop = do
     ShowBindings -> lift get >>= replyAll . allBindings
     (Load lm ss) -> case lm of
       Reset -> reply "TODO"
-      Append -> do
-        parsingLogs <- for ss importModule
-        _ <- for (zip parsingLogs ss) printErrors
-        loop
+      Append -> for ss (lift . importModule) >>= replyAll . concat
     Reload -> reply "TODO"
     Say msg -> reply msg
     Quit -> outputStrLn "Leaving Lambda."
@@ -104,14 +79,8 @@ loop = do
     reply x = outputStrLn (show x) >> loop
     replyAll xs = for_ xs (outputStrLn . show) >> loop
 
-    printErrors (errors, moduleName) = case concat $ map (\err -> show err ++ "\n") errors of
-        [] -> return ()
-        s  -> outputStrLn $ "Errors during import module " ++ moduleName ++ ":\n" ++ s
-
     onBindingResolve _ (Left e) = reply e
-    onBindingResolve nm (Right t) = do
-      lift . modify . writeBinding nm $ bigStep (smallStep normal) t
-      loop
+    onBindingResolve nm (Right t) = lift (writeNormalized nm t) >> loop
 
     onEvalResolve _ _ (Left e) = reply e
     onEvalResolve nl em (Right t) =
@@ -121,3 +90,24 @@ loop = do
        in case em of
             Trace -> replyAll (stepLog step t)
             Silent -> reply (bigStep step t)
+
+------------------------------------ MODULE ------------------------------------
+
+data BindingError = Resolve ResolveError | Parse ParsecError
+
+instance Show BindingError where
+  show (Resolve err) = show err
+  show (Parse err) = errorBundlePretty err
+
+importModule ::
+  (MonadState AppState m, MonadIO m) => String -> m [BindingError]
+importModule moduleName = do
+  (parseErrors, bindings) <- liftIO (parseModule fileName <$> readFile fileName)
+  resolveErrors <- fmap catMaybes . for bindings $ \(Label nm te) -> do
+    state <- get
+    case resolve' te state of
+      Left err -> pure (Just err)
+      Right term -> writeNormalized nm term $> Nothing
+  return $ map Resolve resolveErrors ++ map Parse parseErrors
+  where
+    fileName = "./lib/" ++ moduleName ++ ".lc"
